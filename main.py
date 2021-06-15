7dcbd3f9746a1e1bbef45c34903cff0e23899411
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-06-15 22:47:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 client = commands.Bot(command_prefix="-")
 
 @client.event
-async def on_ready(ctx):
-    await ctx.send("Bot is ready!")
+async def on_ready():
+    print("Bot is ready!")
 
 
 @client.command
